7f491fa9a38cfd0cca4143fcc3eb2e0906f768fd
ascalva <ascalva@gmail.com>
2017-06-16 15:53:48 -0400

    Updated shell script

--- plots/run.sh
+++ plots/run.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
+#
+# filename: run.sh
+#
+# @author: Alberto Serrano
+#
+# purpose: Simple shell script to assist with Gnuplot plots and convert
+#          them into a gif
 
 FILENAME=../coculture.dat
 OUTFILE=co
 EXT=.jpg
-FRAMES=0
+DIRECTORY=./out
+FRAMES=1000
 
-while [ $FRAMES -le 5000 ]; do
+# Function to open gif file
+ql () { qlmanage -p "$*" >& /dev/null; }
+
+while [ $FRAMES -le 9900 ]; do
     gnuplot -e "inst=$FRAMES" -e "filename='$FILENAME'" -e "outfile='$OUTFILE$FRAMES$EXT'" plot.gp
     echo Plotting frame $INST in $OUTFILE$FRAMES$EXT
     FRAMES=$((FRAMES+100))
 done
 
-mkdir out
-mv ./*.jpg ./out/
-open ./out/*.jpg
+# Check if the specified directory exists
+if [ -d "$DIRECTORY" ]; then
+    rm -r $DIRECTORY/*
+else
+    mkdir $DIRECTORY
+fi
+
+mv ./*.jpg $DIRECTORY
+
+# Convert images into a gif (requires ImageMagick)
+convert -delay 10 -loop 10 $DIRECTORY/*.jpg ./coculture.gif
+
+# Open gif
+ql ./coculture.gif
